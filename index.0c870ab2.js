0001662b45114b1a6fee3047730d1fa6e78aa4de
andrii-pozniak <103370189+andrii-pozniak@users.noreply.github.com>
2022-09-17 14:31:18 +0000

    Deploying to gh-pages from @ andrii-pozniak/goit-js-hw-10@47e11f92d4b52ba01d293b18f456220f4f4e6926 🚀

--- index.0c870ab2.js
+++
@@ -1,2 +0,0 @@
-!function(){var n={input:document.querySelector("#search-box"),countryList:document.querySelector(".country-list"),countryInfo:document.querySelector(".country-info")};(function(n){return fetch("https://restcountries.com/v3.1/name/".concat(n,"\n    ")).then((function(n){return n.json}))})(countryName).then().catch((function(n){console.log(n)})),n.input.addEventListener("input",_.debounce((function(){})),300)}();
-//# sourceMappingURL=index.0c870ab2.js.map
